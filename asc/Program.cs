3d2e17e961a55eb49cb072482a7ee76c56d7c912
Vasile Orbai <vasile930@gmail.com>
2020-10-08 22:42:56 -0700

    adaugat citire date

--- asc/Program.cs
+++ asc/Program.cs
@@ -10,7 +10,25 @@ namespace asc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World ! :D");
+            Console.WriteLine("Hello World !");
+            Console.WriteLine("Introduceti numarul de la pantof");
+            
+            string line;
+            line = Console.ReadLine();
+
+            int numarpantof;
+            try
+            {
+                numarpantof = int.Parse(line);
+               
+                Console.WriteLine($"aveti {numarpantof} numarul la pantof");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //int.TryParse(line,out numarpantof)
         }
     }
 }
